b4f601a6f35557cd1102d39b580185201fe0351c
Luke Lin <lukeyw.lin@gmail.com>
2023-11-18 13:46:04 +0800

    add unit test: ArticleViewModel display content

--- Rx-MVVM-DemoTests/Rx_MVVM_DemoTests.swift
+++ Rx-MVVM-DemoTests/Rx_MVVM_DemoTests.swift
@@ -6,24 +6,35 @@
 //
 
 import XCTest
+import RxSwift
+import RxCocoa
 @testable import Rx_MVVM_Demo
 
 final class Rx_MVVM_DemoTests: XCTestCase {
 
+    private let bag = DisposeBag()
+    private var articleVM: RmdArticleViewModeling!
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        articleVM = RmdArticleViewModel()
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+    override func tearDownWithError() throws {}
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testArticleViewModelDisplayContent() throws {
+        let displayName = "sports"
+        let exp = expectation(description: "Article View Model display content")
+        
+        articleVM.subreddit
+            .subscribe(onNext: { subreddit in
+                if subreddit.data.childrens.first?.subreddit == displayName {
+                    exp.fulfill()
+                }
+            }).disposed(by: bag)
+        
+        articleVM.targetSubredditDisplayName.accept(displayName)
+        
+        wait(for: [exp], timeout: 5)
     }
 
     func testPerformanceExample() throws {
